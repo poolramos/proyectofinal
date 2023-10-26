946905f29ff59ee173bbcb01103ea104b638be6e
Pool Ramos <poolramos.unmsm.edu.pe>
2023-10-26 18:49:08 -0500

    first commit

---
+++ src/views/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/dist/output.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <main>
+
+        <?php
+        if (isset($message)) {
+            echo $message;
+        }
+
+        ?>
+        <section class="h-screen bg-logo flex flex-col items-center">
+            <div class="w-72">
+                <img src="/assets/logo.jpg" alt="logo">
+            </div>
+            <div class="bg-white shadow-md w-96 py-8  px-10 ">
+                <h1 class="text-center mb-6">Bienvenido ingresa con tu cuenta</h1>
+
+                <form action="/index.php" method="post" class="flex flex-col gap-3">
+
+                    <div class="border-2 rounded-md ">
+                        <input type="email" id="email" name="email" placeholder="Email" class="w-full p-1 ">
+                    </div>
+
+                    <div class="border-2 rounded-md">
+                        <input type="password" id="password" name="pssword" placeholder="Password" class="w-full p-1 ">
+                    </div>
+
+                    <input type="hidden" name="action" value="login">
+
+                    <div class="flex justify-end">
+                        <button class="bg-blue-500 text-white rounded-md p-2">Ingresar</button>
+                    </div>
+
+                </form>
+            </div>
+
+        </section>
+    </main>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.php", "./*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
